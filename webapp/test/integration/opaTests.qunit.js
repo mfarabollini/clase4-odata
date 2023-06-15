ff7fc86e40eeb3388dc4407da824294a49c6b33b
Marcos farabollini <marcos.farabollini@gmail.com>
2023-06-15 10:25:26 -0300

    initial

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["exaccs/clase4odata/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"exaccs/clase4-odata/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
